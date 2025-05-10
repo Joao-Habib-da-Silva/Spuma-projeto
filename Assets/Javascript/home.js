d7ca965f46df15f12157a8138b0ccd91f3f47528
Joao Habib da Silva <contajh544@gmail.com>
2025-05-10 17:50:58 -0300

    mudanças no estilo da página inicial

---
+++ Assets/Javascript/home.js
@@ -0,0 +1,5 @@
+var home_text = window.document.getElementById("home-text");
+window.onload = function () {
+  home_text.classList.add("start");
+  window.document.getElementById("header").classList.add("start");
+};
