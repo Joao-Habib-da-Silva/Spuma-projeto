1f7dfde2b7083d158429aca74512e9c0e018ecbe
Joao Habib Da Silva <contajh544@gmail.com>
2025-06-06 12:24:34 -0300

    estilizando e tentanto fazer o menu

--- Assets/Javascript/planos.js
+++ Assets/Javascript/planos.js
@@ -1,8 +1,6 @@
 window.onload = function () {
     window.document.getElementById("header").classList.add("start");
-    var title = window.document.getElementById("title")
-    var bar = window.document.getElementById("sobreplanos-bar")
-    title.classList.add("start")
-    bar.classList.add("start")
+    window.document.getElementById("motor").classList.add("start")
+    window.document.getElementById("container-sobre").classList.add("start")
+    window.document.getElementById("menu-for-phones").classList.add("start")
   };
-  
