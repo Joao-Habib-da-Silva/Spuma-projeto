7c11a8eabc8a2092af2783cd96df527ed230587d
Joao Habib da Silva <contajh544@gmail.com>
2025-06-22 20:54:07 -0300

    scroll animação

--- Assets/Javascript/planos.js
+++ Assets/Javascript/planos.js
@@ -4,3 +4,15 @@ window.onload = function () {
     window.document.getElementById("container-sobre").classList.add("start")
     window.document.getElementById("menu-for-phones").classList.add("start")
   };
+  var elements = window.document.getElementById("plano-escolher")
+  window.addEventListener("scroll", function() {
+    var scroll_value = elements.getBoundingClientRect().top
+    var window_height = window.innerHeight
+    if (scroll_value < window_height - 250) {
+      elements.classList.add("scroll-ativo")
+    }
+    else {
+      elements.classList.remove("scroll-ativo")
+    }
+
+  })
