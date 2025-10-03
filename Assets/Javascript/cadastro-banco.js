463e9426baf768de1889f0861b1482b96e6fb38e
Joao Habib da Silva <contajh544@gmail.com>
2025-10-03 11:54:50 -0300

    agora vai irmao

--- Assets/Javascript/cadastro-banco.js
+++ Assets/Javascript/cadastro-banco.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCH7lpKD9aMWorbk_pk3mxlcGXt21GM6lM",
   authDomain: "spuma-banco.firebaseapp.com",
@@ -11,23 +10,19 @@ const firebaseConfig = {
   appId: "1:447336546434:web:23802d28de45fbedc2349b",
   measurementId: "G-4BJ95WYKF5",
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
 const cadastroligar = document.getElementById("button-enviar");
-
 cadastroligar.addEventListener("click", async function () {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha1").value;
   const data = document.getElementById("idade").value;
-
+  const aviso = window.document.getElementById("aviso")
   if (!email || !senha || !data) {
-    alert("Por favor, preencha todos os campos.");
+    aviso.innerHTML = "Preencha todos os campos"
     return;
   }
-
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
     const user = userCredential.user;
@@ -38,7 +33,7 @@ cadastroligar.addEventListener("click", async function () {
       criadoEm: new Date(),
     });
 
-    alert("Usuário cadastrado com sucesso!");
+    aviso.innerHTML = "Cadastrado com sucesso"
   } catch (error) {
     console.error("Erro ao cadastrar:", error.message);
     alert("Erro ao cadastrar: " + error.message);
