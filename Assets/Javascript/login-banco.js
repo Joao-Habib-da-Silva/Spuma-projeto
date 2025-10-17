3f941c83c0f7d3f35abb412f5116cc6e4e68489f
Joao Habib da Silva <contajh544@gmail.com>
2025-10-17 12:00:25 -0300

    login google

--- Assets/Javascript/login-banco.js
+++ Assets/Javascript/login-banco.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import {getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCH7lpKD9aMWorbk_pk3mxlcGXt21GM6lM",
   authDomain: "spuma-banco.firebaseapp.com",
@@ -13,14 +15,35 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const botao = window.document.getElementById("button-enviar")
 const botao_google = window.document.getElementById("google-button")
+const db = getFirestore(app);
 var provider = new GoogleAuthProvider()
+async function criarDocumentoUsuario(user, tipo = "google") {
+    try {
+        await setDoc(doc(db, "users", user.uid), {
+            email: user.email,
+            criadoEm: new Date(), 
+            nascimento: "",
+            telefone: "",
+            cep: "",
+            rua: "",
+            bairro: "",
+            cidade: "",
+            estado: "",
+            provedor: tipo,
+          });
+        console.log("Documento criado para usuário Google:", user.email);
+    } catch (error) {
+        console.error("Erro ao criar documento:", error);
+    }
+}
 botao.addEventListener("click", async function() {
     try {
         const email = window.document.getElementById("email").value
         const senha = window.document.getElementById("senhas").value
         const cred = await signInWithEmailAndPassword(auth, email, senha)
         console.log("user cadastrado")
-    }catch(error) {
+        history.back()
+      }catch(error) {
         console.error(error)
         }
 })
@@ -28,6 +51,8 @@ botao_google.addEventListener("click", async function() {
   try {
     const result = await signInWithPopup(auth, provider)
     const user = result.user
+    await criarDocumentoUsuario(user, "google");
+    history.back()
     return user
   }catch(error) {
     console.error(error)
